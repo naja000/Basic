460eb3c7b6883f5b982c74d2c6d5dce3e2bf4930
naja000 <110969361+naja000@users.noreply.github.com>
2022-08-29 14:50:01 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,32 @@
+for i in range(0,5):
+    for j in range(0,i+1):
+        print("* ",end="")
+    print("\r")
+print("----------------------------")
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(i ,end="")
+    print("\r")
+print("----------------------------")
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j ,end="")
+    print("\r")
+print("----------------------------")
+
+b=0
+for i in range(5,0,-1):
+    b+=1
+    for j in range(1,i+1):
+        print(b,end="")
+    print("\r")
+print("---------------------------")
+
+for i in range(5,0,-1):
+    for j in range(0,i):
+        print(10 ,end="")
+    print("\r")
+
+
